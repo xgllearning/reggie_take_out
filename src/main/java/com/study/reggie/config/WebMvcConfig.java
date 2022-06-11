d6f440b4ca29b1266f46bc741cc12776b4ef7a8b
xgl <34029067@qq.com>
2022-06-11 21:16:22 +0800

    第一次提交：餐饮项目初始环境搭建

---
+++ src/main/java/com/study/reggie/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.study.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+@Slf4j
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始静态资源映射");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
