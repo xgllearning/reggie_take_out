83f8b9e3bfc75114d0df75cdddddcf6fe9a88645
xgl <34029067@qq.com>
2022-06-13 22:24:43 +0800

    第十三次提交：文件的上传和下载

---
+++ src/main/java/com/study/reggie/controller/CommonController.java
@@ -0,0 +1,83 @@
+package com.study.reggie.controller;
+
+import com.study.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 文件上传和下载
+ */
+@Slf4j
+@RequestMapping("common")
+@RestController
+public class CommonController {
+
+    @Value("${reggie.path}")
+    private String basePath;
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
+    public R<String> upload(MultipartFile file){
+        log.info("file:{}",file);
+        //1.获取当前文件的原始名字
+        String originalFilename = file.getOriginalFilename();//xxx.xx
+        //2.获取原始名字的后缀
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));//截取后缀名，包括后缀.xx
+        //组成一个新名字，防止重名覆盖问题
+        String fileName = UUID.randomUUID().toString() + suffix;//或者+originalFilename
+        //创建一个目录对象
+        File dir = new File(basePath);
+        //判断当前目录是否存在
+        if(!dir.exists()){
+            //目录不存在，需要创建
+            dir.mkdirs();
+        }
+        try {
+            //文件转存transferTo("地址");为了解耦，选择配置文件方式填充
+            file.transferTo(new File(basePath + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return R.success(fileName);
+    }
+
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+        try {
+            //输入流，通过输入流读取文件内容
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+
+            //输出流，通过输出流将文件写回浏览器
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            response.setContentType("image/jpeg");
+
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = fileInputStream.read(bytes)) != -1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+            //关闭资源
+            outputStream.close();
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
